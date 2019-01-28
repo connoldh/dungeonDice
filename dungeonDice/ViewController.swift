5e4445dbe302c63ddea490168e0a9155d28c05f2
Jack Connolly <jconnolly226@gmail.com>
2019-01-28 17:49:26 -0500

    Initial Commit

---
+++ dungeonDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dungeonDice
+//
+//  Created by John Connolly on 1/28/19.
+//  Copyright © 2019 John Connolly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
