8437c38f3b2f308c8a746ba620f473e91d6491b9
Jack Connolly <jconnolly226@gmail.com>
2019-01-28 18:37:02 -0500

    first build. no functions yet.

--- dungeonDice/ViewController.swift
+++ dungeonDice/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func fourSided(_ sender: UIButton) {
+        let sides = 4
+        let roll = Int.random(in: 1...sides)
+        print(roll)
+        showMessage.text = "You rolled a \(sides) sided dice and got \(String(roll))"
+        
+    }
+    @IBAction func sixSided(_ sender: UIButton) {
+        let sides = 6
+        let roll = Int.random(in: 1...sides)
+        print(roll)
+        showMessage.text = "You rolled a \(sides) sided dice and got \(String(roll))"
+    }
+    @IBAction func eightSided(_ sender: UIButton) {
+        let sides = 8
+        let roll = Int.random(in: 1...sides)
+        print(roll)
+        showMessage.text = "You rolled a \(sides) sided dice and got \(String(roll))"
+    }
+    @IBAction func tenSided(_ sender: UIButton) {
+        let sides = 10
+        let roll = Int.random(in: 1...sides)
+        print(roll)
+        showMessage.text = "You rolled a \(sides) sided dice and got \(String(roll))"
+    }
+    @IBAction func twelveSided(_ sender: UIButton) {
+        let sides = 12
+        let roll = Int.random(in: 1...sides)
+        print(roll)
+        showMessage.text = "You rolled a \(sides) sided dice and got \(String(roll))"
+    }
+    @IBAction func twentySided(_ sender: UIButton) {
+        let sides = 20
+        let roll = Int.random(in: 1...sides)
+        print(roll)
+        showMessage.text = "You rolled a \(sides) sided dice and got \(String(roll))"
+    }
+    @IBAction func hundredSided(_ sender: UIButton) {
+        let sides = 100
+        let roll = Int.random(in: 1...sides)
+        print(roll)
+        showMessage.text = "You rolled a \(sides) sided dice and got \(String(roll))"
+    }
+    
 }
 
